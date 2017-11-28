7953a5ef05fcc38addbbf41ab5ff76d3f390b1f8
mfaraz <mohammad.faraz@dcsg.com>
2017-11-27 21:45:42 -0500

    django hello wrld

--- polls/templatetags/poll_extras.py
+++ polls/templatetags/poll_extras.py
@@ -1,7 +1,17 @@
 from django import template
-
+import requests
+import json
+from django.http import Http404, HttpResponse
+import datetime
+from django.utils.safestring import mark_safe
 register = template.Library()
 
 @register.simple_tag
 def getEspot():
-    return "yo ssup!!"
+    ur = "url"
+    #response = requests.get('https://api.github.com/user/chao420456')
+    response = requests.get(ur)
+    data = response.json()['MarketingSpotData'][0]['baseMarketingSpotActivityData'][0]
+    data = data['marketingContentDescription'][0]['marketingText']
+    #return data
+    return mark_safe(data)
